7cdf25b2417e0f16c32cec1b187e5f843ce236af
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-06-11 12:45:24 +0000

    [Add]3-12_Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,20 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+  @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to :show(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   def show
@@ -17,6 +27,17 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to 'book_path(@book)'
+    else
+      @books=Book.all
+      render action :edit
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  post 'books' => 'books#create'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_list'
-  patch 'books/:id' => 'list#update', as: 'update_list'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   root to: 'homes#top'
+  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
